3fa9ef927fa4963284c044065895b169501baa2b
Julius Liaudanskis <j.liaudanskis@valantic.nl>
2024-08-08 15:46:48 +0300

    newsletter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import solid from '@astrojs/solid-js';
 
 export default defineConfig({
   site: 'https://astro-frontend-masters-edenofjinx.netlify.app',
+  output: 'hybrid',
+  experimental: {
+    hybridOutput: true,
+  },
   integrations: [
     react({
       include: ['**/react/*']
